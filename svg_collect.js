24b0bf598cfa0f1aa774d8f465e8ee2cff7d8cfe
Your Name <you@example.com>
2017-10-25 14:28:20 +0900

    20170926 fix

--- svg_collect.js
+++ svg_collect.js
@@ -1,8 +1,14 @@
 window.setDevToolsCallback(function(resultJSON) {
-    var jsonObj = JSON.parse(resultJSON);
-    var paramsObj = JSON.parse(jsonObj.params);
+    console.log("resultJSON : " + resultJSON);
 
-    if (paramsObj.idle) {
+    var jsonObj, paramsObj;
+    try {
+      jsonObj = JSON.parse(resultJSON);
+      paramsObj = JSON.parse(jsonObj.params);
+    } catch (exp) {
+    }
+
+    if (paramsObj && paramsObj.idle) {
         setTimeout(function() {
             location.replace(window.location.pathname + "?mode=save&result=" + JSON.stringify(paramsObj));
         }, 100);
